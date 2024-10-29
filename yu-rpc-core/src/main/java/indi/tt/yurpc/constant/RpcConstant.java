bc4df81ce5ccd7711479368a44693cf2e28d1d10
tt <songtaoxie@hotmail.com>
2024-10-29 15:12:40 +0800

    完成全局配置加载

---
+++ yu-rpc-core/src/main/java/indi/tt/yurpc/constant/RpcConstant.java
@@ -0,0 +1,14 @@
+package indi.tt.yurpc.constant;
+
+/**
+ * RPC相关常量
+ *
+ * @author tt
+ */
+public interface RpcConstant {
+
+    /**
+     * 默认配置文件加载前缀
+     */
+    String DEFAULT_CONFIG_PREFIX = "rpc";
+}
